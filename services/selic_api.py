bd0fe62ec56ca6259063d2c02abb9ab0590deb1c
Ítalo Gustavo <42496107+ItaloGustavoS@users.noreply.github.com>
2025-07-28 21:15:33 -0300

    Selic fetch from Brasil API

---
+++ services/selic_api.py
@@ -0,0 +1,15 @@
+import requests
+
+
+def get_selic_atual():
+    url = (
+        "https://api.bcb.gov.br/dados/serie/bcdata.sgs.11/dados/ultimos/1?formato=json"
+    )
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        raise ValueError("Erro ao obter taxa Selic.")
+
+    dados = response.json()[0]
+    selic = float(dados["valor"].replace(",", "."))
+    return round(selic, 2)
