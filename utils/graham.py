2aff4e1704e661e77772e9bf7f454b6e1e9c1368
Ítalo Gustavo <42496107+ItaloGustavoS@users.noreply.github.com>
2025-07-28 21:13:19 -0300

    Graham logic

---
+++ utils/graham.py
@@ -0,0 +1,9 @@
+def calcular_valor_intrinseco(lpa: float, g: float, selic: float) -> float:
+    """
+    Calcula o valor intrínseco de uma ação usando a fórmula modificada de Benjamin Graham.
+    """
+    try:
+        valor_intrinseco = (lpa * (8.5 + 2 * g) * 4.4) / selic
+        return round(valor_intrinseco, 2)
+    except Exception as e:
+        raise ValueError(f"Erro ao calcular valor intrínseco: {e}")
